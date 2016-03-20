f615806766d3dcfdd81d88da9e49be272b9c7957
xTeddyR <teddy.reino9@gmail.com>
2016-03-20 21:56:54 +0100

    Ajout des images et redimentionnement de la fenêtre

--- YVOX/src/main/java/esgi/yvox/Main.java
+++ YVOX/src/main/java/esgi/yvox/Main.java
@@ -1,8 +1,13 @@
 package esgi.yvox;
 
 import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
-
 
 /**
  * Created by Benoit on 17/03/2016.
@@ -15,9 +20,18 @@ public class Main extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
-
         stage.setTitle("YVOX Voice Controller : start state");
+        stage.getIcons().add(new Image("file:img/logo_icone.png"));
+        Rectangle2D mainscreen = Screen.getPrimary().getVisualBounds();
+        stage.setWidth(mainscreen.getWidth() * 0.75);
+        stage.setHeight(mainscreen.getHeight() * 0.75);
 
+        StackPane sp = new StackPane();
+        Image imgmic = new Image("file:img/mic.png");
+        ImageView imgview = new ImageView(imgmic);
+        sp.getChildren().add(imgview);
+        Scene scene = new Scene(sp,imgmic.getWidth(),imgmic.getHeight());
+        stage.setScene(scene);
         stage.show();
     }
 
