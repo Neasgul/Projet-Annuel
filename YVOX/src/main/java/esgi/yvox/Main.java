5e314c1638f789bd47b3932d351433820674d90b
Benoit <benoit.chopinet@sfr.fr>
2016-03-28 11:46:35 +0200

    implementation of vocale recognition (beginning)

--- YVOX/src/main/java/esgi/yvox/Main.java
+++ YVOX/src/main/java/esgi/yvox/Main.java
@@ -1,11 +1,11 @@
 package esgi.yvox;
 
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
 import javafx.application.Application;
-import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -13,12 +13,18 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
-import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 /**
  * Created by Benoit on 17/03/2016.
  */
 public class Main extends Application{
+
+    private Configuration mConfiguration;
+    private LiveSpeechRecognizer lmRecognizer;
+
+    boolean recognitionState;
+
 
     public static void main(String[] args) {
         launch(args);
@@ -26,7 +32,7 @@ public class Main extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("YVOX Voice Controller : start state");
+        stage.setTitle("YVOX Voice Controller");
         stage.getIcons().add(new Image("file:img/logo_icone.png"));
 
         Rectangle2D mainscreen = Screen.getPrimary().getVisualBounds();
@@ -36,20 +42,62 @@ public class Main extends Application{
         StackPane sp = new StackPane();
         Image imgmic = new Image("file:img/mic.png");
         ImageView imgview = new ImageView(imgmic);
-        EventHandler<MouseEvent> img_click = new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                System.out.println("Click event");
-            }
-        };
         imgview.addEventHandler(MouseEvent.MOUSE_PRESSED,img_click);
         sp.getChildren().add(imgview);
         Scene scene = new Scene(sp,imgmic.getWidth(),imgmic.getHeight());
         stage.setScene(scene);
         stage.show();
+        sphinxConfiguration();
     }
 
-    void sphinxConfiguration(){
+    void sphinxConfiguration() throws IOException {
+        mConfiguration = new Configuration();
+
+        mConfiguration
+                .setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+        mConfiguration
+                .setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+        mConfiguration
+                .setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+
+        lmRecognizer = new LiveSpeechRecognizer(mConfiguration);
 
     }
+
+    void onVoiceRecognitionStart(){
+        System.out.println("Speak ... and press the button");
+        recognitionState = true;
+        lmRecognizer.startRecognition(true);
+
+        while (true)
+        {
+            String result = lmRecognizer.getResult().getHypothesis();
+            System.out.println(result);
+            if (result.equals("exit"))
+            {
+                onVoiceRecognitionStop();
+            }
+        }
+
+    }
+
+    void onVoiceRecognitionStop(){
+        recognitionState = false;
+        lmRecognizer.stopRecognition();
+    }
+
+
+    EventHandler<MouseEvent> img_click = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent event) {
+            System.out.println("click event : "+ recognitionState);
+            if (true == recognitionState){
+                onVoiceRecognitionStop();
+            }
+            else{
+                onVoiceRecognitionStart();
+            }
+        }
+    };
+
 }
