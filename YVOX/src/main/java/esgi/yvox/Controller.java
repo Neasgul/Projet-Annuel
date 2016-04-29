b9feb14a2ccfa16d6ee468b80cc33916508e430f
xTeddyR <teddy.reino9@gmail.com>
2016-04-29 15:38:18 +0200

    Add fxml but not implemented yet

---
+++ YVOX/src/main/java/esgi/yvox/Controller.java
@@ -0,0 +1,11 @@
+package esgi.yvox;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+/**
+ * Created by ostro on 29/04/2016.
+ */
+public class Controller {
+
+}
--- YVOX/src/main/java/esgi/yvox/Main.java
+++ YVOX/src/main/java/esgi/yvox/Main.java
@@ -4,7 +4,9 @@ import edu.cmu.sphinx.api.Configuration;
 import edu.cmu.sphinx.api.LiveSpeechRecognizer;
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -33,11 +35,13 @@ public class Main extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
+        //FXMLLoader loader = new FXMLLoader(getClass().getResource("main_scene.fxml"));
+        //Parent main_sc = loader.load();
         stage.setTitle("YVOX Voice Controller");
         stage.getIcons().add(new Image("file:img/logo_icone.png"));
 
         Rectangle2D mainscreen = Screen.getPrimary().getVisualBounds();
-        stage.setWidth(mainscreen.getWidth() * 0.75);
+        stage.setWidth(mainscreen.getWidth() * 0.5);
         stage.setHeight(mainscreen.getHeight() * 0.75);
 
         StackPane sp = new StackPane();
@@ -45,7 +49,8 @@ public class Main extends Application{
         ImageView imgview = new ImageView(imgmic);
         imgview.addEventHandler(MouseEvent.MOUSE_PRESSED,img_click);
         sp.getChildren().add(imgview);
-        Scene scene = new Scene(sp,imgmic.getWidth(),imgmic.getHeight());
+        Scene scene = new Scene(sp,stage.getWidth(),stage.getHeight());
+        //Scene scene = new Scene(main_sc,stage.getWidth(),stage.getHeight());
         stage.setScene(scene);
         stage.show();
         sphinxConfiguration();
@@ -55,12 +60,9 @@ public class Main extends Application{
     void sphinxConfiguration() throws IOException {
         mConfiguration = new Configuration();
 
-        mConfiguration
-                .setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
-        mConfiguration
-                .setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
-        mConfiguration
-                .setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+        mConfiguration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+        mConfiguration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+        mConfiguration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
 
         lmRecognizer = new LiveSpeechRecognizer(mConfiguration);
 
