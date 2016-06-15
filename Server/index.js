3b4df5a401c6ff42f4cb3972657a18c9cad49082
Benoit <benoit.chopinet@sfr.fr>
2016-06-15 12:34:06 +0200

    set base of the ejs use

--- Server/index.js
+++ Server/index.js
@@ -2,11 +2,15 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var favicon = require('serve-favicon');
 var fs = require('fs');
-
 var app = express();
+app.use(express.static(__dirname+'/public'));
+
+//Define the port to use
 var DEFAULT_PORT = 8888;
 var port = process.env.PORT || DEFAULT_PORT
 
+//set the view engine to ejs
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     "extended": false
 }));
@@ -20,13 +24,9 @@ require('./api/Command')(app);
 require('./api/User')(app);
 
 app.get('/',function (req, res) {
-    fs.readFile('Server/public/index.html',function (err, data){
-        res.writeHeader(200, {"Content-Type": "text/html"});
-        res.write(data);
-        res.end();
+    res.render('index');
     });
 
-})
 app.get('/download',function (req, res) {
 var file = __dirname + '/Command.log';
   res.download(file); // Set disposition and send it.
@@ -34,7 +34,8 @@ var file = __dirname + '/Command.log';
 })
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  res.status(404).send('404 : Page Not Found')
+    res.render('404');
+    //res.status(404).send('404 : Page Not Found')
 });
 
 app.listen(port, function () {
