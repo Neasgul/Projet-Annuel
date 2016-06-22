111351a36f307f008eb32f120642c255ee329914
benoit chopinet <benoit.chopinet@invoxia.com>
2016-06-22 13:25:59 +0200

    Use reflecton for uuid annotation

--- YVOX/src/main/java/esgi/yvox/annotation/UUID_Processor.java
+++ YVOX/src/main/java/esgi/yvox/annotation/UUID_Processor.java
@@ -1,6 +1,8 @@
 package esgi.yvox.annotation;
 
 import java.io.*;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AnnotatedArrayType;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.file.*;
@@ -12,29 +14,31 @@ import java.util.UUID;
  */
 public class UUID_Processor {
     public UUID_Processor(Class clazz) {
-        String cur_dir = System.getProperty("user.dir");
-        Path path = Paths.get(cur_dir);
-        try{
-            DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path);
-            Iterator<Path> iterator = directoryStream.iterator();
-            int count = 0;
-            while (iterator.hasNext()){
-                Path path1 = iterator.next();
-                if(path1.endsWith("token.txt")){
-                    count++;
-                    break;
+        if(!clazz.isAnnotationPresent(esgi.yvox.annotation.UUID.class)){
+            String cur_dir = System.getProperty("user.dir");
+            Path path = Paths.get(cur_dir);
+            try{
+                DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path);
+                Iterator<Path> iterator = directoryStream.iterator();
+                int count = 0;
+                while (iterator.hasNext()){
+                    Path path1 = iterator.next();
+                    if(path1.endsWith("token.txt")){
+                        count++;
+                        break;
+                    }
                 }
+                directoryStream.close();
+                if (count == 0) {
+                    UUID uuid = UUID.randomUUID();
+                    FileWriter fw = new FileWriter("token.txt");
+                    fw.write(uuid.toString());
+                    fw.close();
+                    sendNewUser(uuid.toString());
+                }
+            }catch (Exception ex){
+                ex.printStackTrace();
             }
-            directoryStream.close();
-            if (count == 0) {
-                UUID uuid = UUID.randomUUID();
-                FileWriter fw = new FileWriter("token.txt");
-                fw.write(uuid.toString());
-                fw.close();
-                sendNewUser(uuid.toString());
-            }
-        }catch (Exception ex){
-            ex.printStackTrace();
         }
     }
 
