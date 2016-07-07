512c0c5278bc08d486b74d11e227eac8cbd5053e
Benoit <benoit.chopinet@sfr.fr>
2016-07-07 14:45:18 +0200

    fix logo not showing

--- YVOX/src/main/java/esgi/yvox/Main.java
+++ YVOX/src/main/java/esgi/yvox/Main.java
@@ -31,7 +31,7 @@ public class Main extends Application{
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/main_scene.fxml"));
         Parent main_sc = loader.load();
         stage.setTitle("YVOX Voice Controller");
-        stage.getIcons().add(new Image("file:img/logo_icone.png"));
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/img/logo_icone_transparent.png")));
 
         Rectangle2D mainscreen = Screen.getPrimary().getVisualBounds();
         stage.setWidth(mainscreen.getWidth() * 0.5);
