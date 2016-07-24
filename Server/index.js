7006a50c0e2da7f123ecfa4fbf8963193734881b
Benoit <benoit.chopinet@sfr.fr>
2016-07-24 20:32:31 +0200

    Change download target

--- Server/index.js
+++ Server/index.js
@@ -45,7 +45,7 @@ app.get('/plugins',function (req, res) {
 
 })
 app.get('/download/',function (req, res) {
-var file = __dirname + '/Command.log';
+var file = __dirname + '/YVOX/YVOX.jar';
   res.download(file); // Set disposition and send it.
 
 })
