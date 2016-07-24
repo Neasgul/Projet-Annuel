b35703fcf594e9224e6c750e487a752182c2a7e7
Benoit <benoit.chopinet@sfr.fr>
2016-07-24 20:32:03 +0200

    fix test

--- YVOX/src/test/java/esgi/yvox/test/CommandDetectionTest.java
+++ YVOX/src/test/java/esgi/yvox/test/CommandDetectionTest.java
@@ -5,6 +5,7 @@ import esgi.yvox.Command_Manager;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import static org.junit.Assert.*;
@@ -20,7 +21,33 @@ public class CommandDetectionTest {
     @Before
     public void SetUp(){
 
-        command_manager = new Command_Manager(new HashMap());
+        HashMap<Integer, ArrayList<String>> keyword_map = new HashMap<>();
+        ArrayList<String> words = new ArrayList<>();
+        words.add("exit");
+        words.add("stop");
+        words.add("cancel");
+
+        keyword_map.put(0, words);
+
+        words = new ArrayList<>();
+        words.add("execute");
+        words.add("open");
+        words.add("start");
+        keyword_map.put(1, words);
+
+        words = new ArrayList<>();
+        words.add("create");
+        words.add("make");
+        //words.add("cancel");
+        keyword_map.put(2, words);
+
+        words = new ArrayList<>();
+        words.add("delete");
+        words.add("remove");
+        words.add("suppress");
+        keyword_map.put(3, words);
+
+        command_manager = new Command_Manager(keyword_map);
     }
     @Test
     public void testCreationCommand(){
